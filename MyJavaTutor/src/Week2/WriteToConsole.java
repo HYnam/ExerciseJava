ad8efaddc9b41dc2ddea57f02ecc9d3e5ed059eb
HYnam <namhy2006@gmail.com>
2021-08-07 17:12:57 +0800

    Write to the console

---
+++ MyJavaTutor/src/Week2/WriteToConsole.java
@@ -0,0 +1,37 @@
+package Week2;
+
+public class WriteToConsole
+{
+    /**
+     * Prints the phrase "Hello, user!" to the system out console.
+     */
+    public void printHelloUser()
+    {
+        // write your code here
+        System.out.println("Hello, user!");
+    }
+
+    /**
+     * Prints the phrase "Hello, user!" to the system out console (i.e. the first line),
+     * followed by "Greetings!" on a new line (i.e. the second line).
+     */
+    public void printHelloUserNewline() {
+        // write your code here
+        System.out.println("Hello, user!");
+        System.out.println("Greetings!");
+    }
+
+    /**
+     * Prints the phrase "Hello, user!" to the system out console (i.e. the first line),
+     * followed by two blank lines,
+     * followed by "Greetings!" on the fourth line.
+     */
+    public void printHelloUserBlankLines()
+    {
+        // write your code here
+        System.out.println("Hello, user!");
+        System.out.println();
+        System.out.println();
+        System.out.println("Greetings!");
+    }
+}
