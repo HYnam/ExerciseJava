e3bf0bb5e12847b52a71ffcfb77b51f2f7aa6ee3
MilaHY <namhy2006@gmail.com>
2021-11-19 12:36:09 +0800

    abstract Employee class with constructor and getter methods

---
+++ MyJavaTutor/src/Week3/inheritanceemployee/Employee.java
@@ -0,0 +1,20 @@
+package Week3.inheritanceemployee;
+
+public abstract class Employee {
+
+    private String name;
+    private double wage;
+
+    public Employee(String name, double wage) {
+        this.name = name;
+        this.wage = wage;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getWage() {
+        return this.wage;
+    }
+}
