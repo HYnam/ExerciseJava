2c391f8f450eda5673060c4d5958cc9ef95e7bc1
MilaHY <namhy2006@gmail.com>
2021-11-19 12:27:49 +0800

    Abstract class of animal and methods

--- MyJavaTutor/src/Week3/inheritancebasic/Animal.java
+++ MyJavaTutor/src/Week3/inheritancebasic/Animal.java
@@ -1,4 +1,21 @@
 package Week3.inheritancebasic;
 
-public class Animal {
+public abstract class Animal {
+    String animal_name;
+    int animal_age;
+
+    public Animal(String name, int age) {
+        this.animal_name = name;
+        this.animal_age = age;
+    }
+
+    public String getName() {
+        return this.animal_name;
+    }
+
+    public int getAge() {
+        return this.animal_age;
+    }
+
+    public abstract String communication();
 }
